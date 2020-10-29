080aaa492e12b4a80c7206665c17c99d652fd634
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2020-10-29 11:00:31 +0100

    Create a mapping instead of an array

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -11,18 +11,17 @@ contract helloWorld{
         uint age;
         // Make a uint height
         uint height;
-        // Make an address creator
-        address creator;
     }
     
-    // Make a private people array out of the person struct
-    person[] private people;
-    
-    
-    // creates a person with the given name, age and height
+    // Creates a private people mapping with the address that gives back a person
+    mapping(address => person) private people;
+
+    // Creates a person with the given name, age and height
     function createPerson(string memory _name, uint _age, uint _height) public{
-        
-        // creates the new person from the Person struct stored in memory
+        // The creators address is = to their own address
+        address creator = msg.sender;
+
+        // Creates the new person from the person struct stored in memory
         person memory newPerson;
         // The name of the new person is = to the function input _name
         newPerson.name = _name;
@@ -30,17 +29,17 @@ contract helloWorld{
         newPerson.age = _age;
         // The height of the new person is = to the function input _height
         newPerson.height = _height;
-        // The creators address is = to their address
-        newPerson.creator = msg.sender;
-        
-        // Push the new person into the array people
-        people.push(newPerson);
+
+        // The creators address of the people mapping is = to the new person
+        people[creator] = newPerson;
     }
     
-    //gets a person with their id and returns a string, uint, uint and an address
-    function getPerson(uint id) public view returns (string memory, uint, uint, address){
-        // returns the persons name, age, height and the creators address with the id of the array people
-        return (people[id].name, people[id].age, people[id].height, people[id].creator);
+    // Gets a person and returns the string name, uint age and uint height
+    function getPerson() public view returns(string memory _name, uint _age, uint _height){
+        // The creators address is = to their own address
+        address creator = msg.sender;
+        // returns the persons name, age and height of the people mapping with the creators address 
+        return (people[creator].name, people[creator].age, people[creator].height);
     }
     
 }
