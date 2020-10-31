ca4d0310a61fdfdebff10ccccc5ac5e29164e9ea
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2020-10-31 17:16:45 +0100

    Update HelloWorld.sol
    add array, change mapping and add function returnIds

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -4,7 +4,9 @@ pragma solidity 0.5.12;
 contract helloWorld{
     
     // Make a struct person
-    struct person {
+    struct Person {
+        // Make an address creator
+        address creator;
         // Make a string name
         string name;
         // Make a uint age
@@ -13,16 +15,19 @@ contract helloWorld{
         uint height;
     }
     
-    // Creates a private people mapping with the address that gives back a person
-    mapping(address => person) private people;
+    // Creates a public numOfPeopleByAddr mapping with the address that gives back a uint
+    mapping(address => uint) public numOfPeopleByAddr;
+    
+    // Create a private people array with the Person struct
+    Person[] private people;
 
     // Creates a person with the given name, age and height
     function createPerson(string memory _name, uint _age, uint _height) public{
-        // The creators address is = to their own address
-        address creator = msg.sender;
 
         // Creates the new person from the person struct stored in memory
-        person memory newPerson;
+        Person memory newPerson;
+        // The creators address is = their own address
+        newPerson.creator = msg.sender;
         // The name of the new person is = to the function input _name
         newPerson.name = _name;
         // The age of the new person is = to the function input _age
@@ -31,15 +36,39 @@ contract helloWorld{
         newPerson.height = _height;
 
         // The creators address of the people mapping is = to the new person
-        people[creator] = newPerson;
+        people.push(newPerson);
+        
+        // The users address of the numOfPeopleByAddr mapping += 1
+        numOfPeopleByAddr[msg.sender] += 1;
+        
     }
     
     // Gets a person and returns the string name, uint age and uint height
-    function getPerson() public view returns(string memory _name, uint _age, uint _height){
-        // The creators address is = to their own address
-        address creator = msg.sender;
+    function getPerson(uint id) public view returns (string memory _name, uint _age, uint _height, address creator){
+
         // returns the persons name, age and height of the people mapping with the creators address 
-        return (people[creator].name, people[creator].age, people[creator].height);
+        return (people[id].name, people[id].age, people[id].height, people[id].creator);
+    }
+    
+    // Make a function returnIds and returns the uint array memory
+    function returnIds() private view returns (uint[] memory){
+        
+        // The uint array memory result = to the new uint array numOfPeopleByAddr mapping from the users address
+        uint[] memory result = new uint[](numOfPeopleByAddr[msg.sender]);
+        // uint creator
+        uint k;
+        // The loop starts at 0 and ends at the max length of the people array
+        for (uint i=0; i<people.length;i++) {
+            // if the people index is == to the users address
+            if(people[i].creator == msg.sender) {
+                // The result of the creator is = to the index
+                result[k] = i;
+                // creator +1
+                k++;
+            }
+        }
+        // Returns the result
+        return result;
     }
     
 }
