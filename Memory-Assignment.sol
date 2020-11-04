81f1c16abcb79d03b54c12b2969c6bdc250c76fb
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2020-11-04 10:49:52 +0100

    Update Memory-Assignment.sol
    Add comments

--- Memory-Assignment.sol
+++ Memory-Assignment.sol
@@ -1,24 +1,36 @@
 pragma solidity 0.5.12;
 
+// Make a contract MemoryAndStorage
 contract MemoryAndStorage {
     
+    // Make a users mapping with a uint pointing to the User struct
     mapping(uint => User) users;
     
+    // Make a struct User
     struct User{
+        // Make a uint id 
         uint id;
+        // Make uint balance
         uint balance;
     }
     
+    // Create a function to add a user with their id and balance
     function addUser(uint id, uint balance) public {
+        // The id of the users mapping = User struct with the id and balance
         users[id] = User(id, balance);
     }
     
+    // Create a function to uppdate the balance
     function updateBalance(uint id, uint balance) public {
+        // Create a user out of the User storage = to the users id
         User storage user = users[id];
+        // The user balance is = to the input balance
         user.balance = balance;
     }
     
+    // Create a function that gets the balance of the inputtet id
     function getBalance(uint id) view public returns(uint) {
+        // Returns the balance of the users id
         return users[id].balance;
     }
     
