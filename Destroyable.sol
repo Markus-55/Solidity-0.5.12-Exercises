fd080969f98d4a7eabd628069194eaa1b78dc54e
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2020-11-26 11:04:59 +0100

    Create Destroyable.sol

---
+++ Destroyable.sol
@@ -0,0 +1,14 @@
+// Import the file Ownable.sol
+import "./Ownable.sol";
+
+pragma solidity 0.5.12;
+
+// Make a Destroyable contract that inherits from the Ownable contract
+contract Destroyable is Ownable {
+    
+    // Make a function to selfdestruct, only accessible by the owner
+    function destroy() public onlyOwner {
+        // Selfdestruct using the users address
+        selfdestruct(msg.sender);
+    }
+}
