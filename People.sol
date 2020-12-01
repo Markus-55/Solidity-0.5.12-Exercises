17359c0e925999adc67a703bedacf218a6caf027
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2020-12-01 15:00:28 +0100

    add deletePerson function

--- People.sol
+++ People.sol
@@ -1,7 +1,23 @@
 pragma solidity 0.5.12;
 
-// Make contract HelloWorld
-contract HelloWorld{
+// Make a People contract
+contract People{
+    
+    // Make an address owner
+    address public owner;
+    
+    // Make a constructor
+    constructor() public {
+        // The owner is = to users address
+        owner = msg.sender;
+    }
+    
+    // Make a modifier onlyOwner
+    modifier onlyOwner {
+        // Require the users address to be == to the owner
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
     
     // Make a struct Person
     struct Person {
@@ -13,17 +29,16 @@ contract HelloWorld{
         uint height;
     }
     
+    // Make a people mapping with the address that gives back a person 
+    mapping(address => Person) public people;
+    
     // Create an event when a person is created with the name, age and height
     event personCreated(string name, uint age, uint height);
-    
     // Make an event when a person is updated with the old name, age and height & the new name, age and height
     event personUpdated(string oldName, uint oldAge, uint oldHeight, string name, uint age, uint height);
     
-    // Make a people mapping with the address that gives back a person 
-    mapping(address => Person) private people;
-    
-    // Make a function that creates a person with a name, age, height
-    function createPerson(string memory _name, uint _age, uint _height) public{
+    // Make a function that creates a person with a name, age and height
+    function createPerson(string memory _name, uint _age, uint _height) internal {
     
     // If the age of the people in the mapping with the users address is == to 0
     if(people[msg.sender].age == 0){
@@ -43,7 +58,7 @@ contract HelloWorld{
         string memory oldName = people[msg.sender].name;
         // The uint old age = to the age in the people mapping with the users address
         uint oldAge = people[msg.sender].age;
-        // The uint old height = to the 
+        // The uint old height = to the height in the people mapping with the users address
         uint oldHeight = people[msg.sender].height;
         
         // The name of the people in the mapping with the users address is = to the input name
@@ -57,12 +72,19 @@ contract HelloWorld{
         emit personUpdated(oldName, oldAge, oldHeight, people[msg.sender].name, people[msg.sender].age, people[msg.sender].height);
    
         }
-        
     }
-    // Make a function getPerson that returns a string memory, and two uint
-    function getPerson () public view returns (string memory, uint, uint) {
+    
+    // Make a function to get a person
+    function getPerson () public view returns(string memory, uint, uint) {
         // Return the name, age and height of the people mapping with the users address
         return (people[msg.sender].name, people[msg.sender].age, people[msg.sender].height);
     }
-
+    
+    // Make a function to delete a person with the creator address
+    function deletePerson(address _creator) public {
+        // delete the creator in people mapping
+        delete people[_creator];
+        // Assert that the age of the people with the creator address is = to 0
+        assert(people[_creator].age == 0);
+    }
 }
