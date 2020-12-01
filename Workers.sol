819c7ff332d6bd850147dac8635955e48a9ed8d2
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2020-12-01 15:04:12 +0100

    Change functions and remove struct

--- Workers.sol
+++ Workers.sol
@@ -1,50 +1,39 @@
 pragma solidity 0.5.12;
 
-// Import the file HelloWorld.sol
-import "./HelloWorld.sol";
+// Import the file People.sol
+import "./People.sol";
 
 // Make a contract Workers inherit from the people contract
 contract Workers is People {
     
-    // Make a struct wallet
-    struct wallet {
-        // Make an address hash
-        address hash;
-        // Make a uint money
-        uint money;
-    }
-    
-    // Make an instance from the people contract = to people contract address
-    People instance = People(0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B);
-    
     // Make a salary mapping with an address that points to a uint
     mapping(address => uint) public salary;
+    // Make a bossOf mapping with an address that points to a address
+    mapping(address => address) public bossOf;
 
-    // Make a function to create a worker with a name, age, height and salary
-    function createWorker(string memory _name, uint _age, uint _height, uint _salary) public {
-        // Take the instance of the createPerson function in the people contract
-        instance.createPerson(_name, _age, _height);
+    // Make a function to create a worker with a name, age, height, salary and the boss address
+    function createWorker(string memory _name, uint _age, uint _height, uint _salary, address _boss) public {
+        // Require the age to not exceed 75 and that the boss is not = to users address
+        require(_age <= 75 && _boss != msg.sender);
         
-        // Require the age to not exceed 75
-        require(_age <= 75, "Age above 75 is not allowed");
-        
-        // The address of the worker is = to the users address
-        address worker = msg.sender;
-        
-        // Create a new salary out of the wallets memory
-        wallet memory newSalary;
-        // The new salary with the hash is = to the worker address
-        newSalary.hash = worker;
-        // The new salary with the money is = to the worker salary
-        newSalary.money = _salary;
-        
-        // The worker in the salary mapping is = to the worker salary
-        salary[worker] = _salary;
+        // Call function createPerson with a name, age and height as argument
+        createPerson(_name, _age, _height);
+        // The users address in the salary mapping is = to the input salary
+        salary[msg.sender] = _salary;
+        // The users address in the bossOf mapping is = to the input boss
+        bossOf[msg.sender] = _boss;
     }
     
     // Make a function to fire worker with their address
-    function fire(address worker) public {
-        // Delete the worker in the salary mapping
-        delete salary[worker];
+    function fire(address _workerAddress) public {
+        // Require that the users addres is the boss of the worker address
+        require(msg.sender == bossOf[_workerAddress]);
+        
+        // Call function deletePerson with the worker address as an argument
+        deletePerson(_workerAddress);
+        // delete the worker address in the salary mapping
+        delete(salary[_workerAddress]);
+        // delete the worker address in the bossOf mapping
+        delete(bossOf[_workerAddress]);
     }
 }
