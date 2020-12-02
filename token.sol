7b7ad0fdddc1ad3f1d78de28fed7cabe22f1f574
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2020-12-02 15:05:50 +0100

    Add SafeMath library

--- token.sol
+++ token.sol
@@ -2,40 +2,34 @@ pragma solidity 0.5.12;
 
 // Import file Ownable.sol
 import "./Ownable.sol";
+// Import file Safemath.sol
+import "./Safemath.sol";
 
-contract ERC20 {
+contract ERC20 is Ownable {
+    
+    using SafeMath for uint256;
+    
     // Make a string variable name
     string private name_;
     // Make a string variable symbol
     string private symbol_;
     // Make a uint8 variable decimals
-    uint8 private decimals_;
+    uint256 private decimals_;
     
     // Make a uint256 variable total supply
     uint256 private totalSupply_;
-    
-    // Make an address minter;
-    address public minter;
     
     // Make a mapping of the balances with an address that point to a uint256
     mapping(address => uint256) private balances;
     
     // Make a constructor with a string name and symbol, a uint8 decimals and a uint256 total
-    constructor(string memory name, string memory symbol, uint8 decimals, uint256 total) public {
+    constructor(string memory name, string memory symbol, uint256 decimals) public {
         // The string variable name is = to the input name
         name_ = name;
         // The string variable symbol is = to the input symbol
         symbol_ = symbol;
         // The uint8 variable decimals is = to the input decimals
         decimals_ = decimals;
-        
-        // The uint256 variable total supply is = to the input total
-        totalSupply_ = total;
-        // The users address in the balances mapping is = to the uint256 variable total supply
-        balances[msg.sender] = totalSupply_;
-        
-        // The address minter is = to the users address
-        minter = msg.sender;
     }
     
     // Make a function name
@@ -51,7 +45,7 @@ contract ERC20 {
     }
     
     // Make a function decimals
-    function decimals() public view returns(uint8) {
+    function decimals() public view returns(uint256) {
         // Returns the uint8 variable decimals
         return decimals_;
     }
@@ -69,26 +63,31 @@ contract ERC20 {
     }
     
     // Make function mint with the account address and the token amount
-    function mint(address _account, uint256 _amount) public {
-        // Require the users address to be the minter;
-        require(msg.sender == minter);
-        // Require the input token amount to be < than 1 * 10 ^ 60
-        require(_amount < 1e60);
+    function mint(address _account, uint256 _amount) public onlyOwner {
+        // Require that the account address is not = to the 0 address
+        require(_account != address(0), "mint to the zero address");
+        
+        // The total supply is = to the total supply + the input token amount
+        totalSupply_ = totalSupply_.add(_amount);
         // The input account address in the balances mapping 
         // is = the input account address in the balances mapping + the input token amount
-        balances[_account] = balances[_account] + _amount;
+        balances[_account] = balances[_account].add(_amount);
     }
     
     // Make a function transfer with a recipient address and the token amount
     function transfer(address _recipient, uint256 _amount) public returns(bool) {
-        // require users address in the balances mapping to point to the input token amount
-        require(_amount <= balances[msg.sender]);
+        // Require that the recipient address is not = to the 0 address
+        require(_recipient != address(0), "transfer to the zero address");
+        // Require that the msg.sender in the balances mapping is >= to the input amount
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        
         // The users address in the balances mapping
         // is = to the users address in the balances mapping - the input token amount
-        balances[msg.sender] = balances[msg.sender] - _amount;
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
         // The input recipient address in the balances mapping
         // is = to the input recipient address in the balances mapping + the input token amount
-        balances[_recipient] = balances[_recipient] + _amount;
+        balances[_recipient] = balances[_recipient].add(_amount);
+        
         // Returns true
         return true;
     }
